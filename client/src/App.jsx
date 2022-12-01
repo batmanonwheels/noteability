c3c2cef0e969bf900ee3b44f8622ff28dd947c9f
Devin Garcia <devingarcia5423@gmail.com>
2022-12-01 18:27:02 -0500

    finished game settings functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,26 +4,49 @@ import pianoLogoW from './assets/piano_white.png';
 import './App.css';
 import Piano from './components/Piano';
 import SongCard from './components/SongCard';
+import GameSettings from './components/GameSettings';
 import Rules from './components/modals/Rules';
 import { MdLightMode, MdDarkMode } from 'react-icons/md';
 
 function App() {
   // const [key, setKey] = useState('none');
+  const [isPlayClicked, setIsPlayClicked] = useState(false);
   const [isRulesClicked, setIsRulesClicked] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const goodKeys = [
+    'a',
+    'w',
+    's',
+    'e',
+    'd',
+    'f',
+    't',
+    'g',
+    'y',
+    'h',
+    'u',
+    'j',
+    'k',
+    'o',
+    'l',
+    'p',
+    ';',
+  ];
 
   const handleKeyPress = (e) => {
-    if (!!e.key) {
+    if (!!e.key && goodKeys.indexOf(e.key) > -1) {
       const element = document.querySelector(
         `#${e.key === ';' ? 'semicolon' : e.key}`
       );
+      // console.log(element);
       element.classList.toggle('playing');
       setTimeout(() => {
         element.classList.toggle('playing');
       }, '100');
     } else {
-      return;
+      a;
     }
+
     let sounds = document.querySelectorAll('audio');
     for (let sound of sounds) {
       // console.log(sound.getAttribute('data-note'), !sound.paused);
@@ -63,14 +86,19 @@ function App() {
     inputElement.style.visibility = 'hidden'; // hide it again
   };
 
+  const handlePlayClick = () => {
+    setIsPlayClicked(true);
+    // console.log(isPlayClicked);
+  };
+
   const handleRulesClick = () => {
     setIsRulesClicked(true);
-    console.log(isRulesClicked);
+    // console.log(isRulesClicked);
   };
 
   const handleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
-    console.log(isDarkMode);
+    // console.log(isDarkMode);
     let element = document.querySelector('.app');
     element.classList.toggle('dark-mode');
   };
@@ -79,30 +107,38 @@ function App() {
     <div tabIndex={0} onKeyDown={(e) => handleKeyPress(e)} className='app'>
       {!isRulesClicked ? (
         <>
-          <input style={{ visibility: 'hidden' }} />
-          <div>
-            <img
-              src={!isDarkMode ? pianoLogoB : pianoLogoW}
-              className='logo'
-              alt='Piano logo'
-            />
-          </div>
-          <h1>NoteAbility</h1>
-          <h2>the Name that Song Game!</h2>
-          {/* {key !== 'none' && key !== 'Meta' ? <h3>key is {key}</h3> : null} */}
-          <div className='menu-buttons'>
-            <button className='menu-button'>Play</button>
-            <button className='menu-button' onClick={handleRulesClick}>
-              How To Play
-            </button>
-            <button className='menu-button dark-mode-toggle'>
-              {isDarkMode ? (
-                <MdLightMode onClick={handleDarkMode} />
-              ) : (
-                <MdDarkMode onClick={handleDarkMode} />
-              )}
-            </button>
-          </div>
+          {isPlayClicked ? (
+            <GameSettings isDarkMode={isDarkMode} />
+          ) : (
+            <>
+              <input style={{ visibility: 'hidden' }} />
+              <div>
+                <img
+                  src={!isDarkMode ? pianoLogoB : pianoLogoW}
+                  className='logo'
+                  alt='Piano logo'
+                />
+              </div>
+              <h1>NoteAbility</h1>
+              <h2>the Name that Song Game!</h2>
+              {/* {key !== 'none' && key !== 'Meta' ? <h3>key is {key}</h3> : null} */}
+              <div className='menu-buttons'>
+                <button className='menu-button' onClick={handlePlayClick}>
+                  Play
+                </button>
+                <button className='menu-button' onClick={handleRulesClick}>
+                  How To Play
+                </button>
+                <button className='menu-button dark-mode-toggle'>
+                  {isDarkMode ? (
+                    <MdLightMode onClick={handleDarkMode} />
+                  ) : (
+                    <MdDarkMode onClick={handleDarkMode} />
+                  )}
+                </button>
+              </div>
+            </>
+          )}
           <Piano handleKeyPress={handleKeyPress} />
           {/* <div className='card'>
             {navigator.userAgent.toLowerCase().includes('macintosh') ? null : (
@@ -122,3 +158,32 @@ function App() {
 }
 
 export default App;
+// if (goodKeys.indexOf(e.key) > -1 || !!e.target.getAttribute) {
+//   if (!!e.key && goodKeys.indexOf(e.key) > -1) {
+//     let element = document.querySelector(
+//       `#${e.key === ';' ? 'semicolon' : e.key}`
+//     );
+//     console.log(element);
+//     element.classList.toggle('playing');
+//     setTimeout(() => {
+//       element.classList.toggle('playing');
+//     }, '100');
+//   }
+//   let sounds = document.querySelectorAll('audio');
+//   for (let sound of sounds) {
+//     // console.log(sound.getAttribute('data-note'), !sound.paused);
+//     if (!sound.paused) {
+//       sound.pause();
+//       sound.currentTime = 0;
+//     }
+//   }
+//   let audio = !!e.key
+//     ? document.querySelector(`audio[data-note="${e.key}"]`)
+//     : document.querySelector(
+//         `audio[data-note="${e.target.getAttribute('data-note')}"]`
+//       );
+//   if (audio !== null) {
+//     // console.log('Not Null');a
+//     playMedia(audio);
+//   }
+// }
